52b4e3f55790998d976c22b78598252dbd5630d7
rex-i <91949903+rex-i@users.noreply.github.com>
2021-10-05 23:49:02 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,17 @@
 ## The function will be used to create a matrix and customized objects within
 ## The makecachematrix is a function in which would create a matrix for it to be identified
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {13213,23)
         f <- NULL
         set <-function(y) {
                 x <<- y
                 f <<= NULL
-}
-get <- function() x
-setinverse <- function(inverse) f <<- inverse
-getinverse <- function() f
-list(set = set,
-     get = get,
-     setinverse = setinverse,
-     getinverse = getinverse)
+                function(X) [
       
 }
 
 
 ## This function will solve the matrx's inversed values
 ## the inverse's value will be calculated by the chache using cacheSolve function
-
-cacheSolve <- function(x, ...) {
-        f <- x$getinverse()
-if (!is.null(f)) {
-        message("getting cached data")
-                return(f)
-}
-        data <- x$get()
-        f <- solve(data, ...)
-        x$setinverse(f)
-        f
-}
-         
+cached data = week 3 
+                    
+= null } development: 
