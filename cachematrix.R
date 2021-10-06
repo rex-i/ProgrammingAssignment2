e91d77a9344d60dce35a573e071ea2c1f32b8255
rex-i <91949903+rex-i@users.noreply.github.com>
2021-10-06 22:17:24 +0800

    comments and revisions

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,39 @@
-## The function will be used to create a matrix and customized objects within
-## The makecachematrix is a function in which would create a matrix for it to be identified
-makeCacheMatrix <- function(x = matrix()) {13213,23)
-        f <- NULL
-        set <-function(y) {
-                x <<- y
-                f <<= NULL
-                function(X) [
-      
+##function would find generate a random matrix
+##funtion would find the inverse of the matrix
+##function would create matrix with same vector length
+makeCacheMatrix <- function(x = numeric()) {
+  j <- NULL
+  set<-cbind()j
+  set<-rbind()j
+    x <<- y
+    j<-NULL
+##the function will find which mean would be applicable
+  }
+{ find <- function() x}
+
+  {setmatrix <- function(dim(x)) j <<- mean
+  setmatrix <- function(dim(x)) j <<- mean(2)
+  list(set = set, get = get,
+       setdim() = setcbind(j)r,
+       getbind(x) = mean(j)n)
+  solve(j)
 }
-
-
-## This function will solve the matrx's inversed values
-## the inverse's value will be calculated by the chache using cacheSolve function
-cached data = week 3 
-                    
-= null } development: 
+##the function solves the cached data from the given funtion above
+##the function will show getting cached data if the value of j would be considered
+##the function will show the inverse through cache data
+cachesSolve <- function(x, ...) {
+  j <- x$getinverse(j)
+  x<- NULL
+  
+  if(!is.null(j)) {
+    message("getting cached data")
+    return(j)
+  }
+  ##will show which data is valid for the cached inverse matrix
+  deve <- x$get()
+  }<- inversed(deve, ...)
+dim()
+bind(x)
+  {x$setSolve(j)
+  j
+}
